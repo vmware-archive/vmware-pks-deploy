eeda3d34fbf07640403494da63305beeeb2941cb
John Gardner <huxoll@gmail.com>
2018-06-20 00:42:22 -0500

    Add fly API fetch to concourse playbook. Signed-off-by: John Gardner <huxoll@gmail.com>

--- downloads/download.sh
+++ downloads/download.sh
@@ -6,9 +6,14 @@ docker run  -v $PWD:/vmwfiles apnex/myvmw
 docker run -v ${PWD}:/vmwfiles apnex/myvmw "VMware Pivotal Container Service"
 docker run -v ${PWD}:/vmwfiles apnex/myvmw get nsx-unified-appliance-2.1.0.0.0.7395503.ova
 docker run -v ${PWD}:/vmwfiles apnex/myvmw get nsx-controller-2.1.0.0.0.7395493.ova
-docker run -v ${PWD}:/vmwfiles apnex/myvmw get nsx-edge-2.1.0.0.0.7395502.ova 
+docker run -v ${PWD}:/vmwfiles apnex/myvmw get nsx-edge-2.1.0.0.0.7395502.ova
 
-wget https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.51/pivnet-linux-amd64-0.0.51
-chmod +x pivnet-linux-amd64-0.0.51
-./pivnet-linux-amd64-0.0.51 login --api-token $API_TOKEN
-./pivnet-linux-amd64-0.0.51 download-product-files -p ops-manager -r 2.1.5 -g '*vsphere*'
+curl -L -o /usr/local/bin/pivnet-cli https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.51/pivnet-linux-amd64-0.0.51
+chmod +x /usr/local/bin/pivnet-cli
+
+if [[ -z "$API_TOKEN" ]]; then
+    echo "Must provide a Pivotal Network API_TOKEN in environment" 1>&2
+    exit 1
+fi
+/usr/local/bin/pivnet-cli login --api-token $API_TOKEN
+/usr/local/bin/pivnet-cli download-product-files -p ops-manager -r 2.1.5 -g '*vsphere*'
