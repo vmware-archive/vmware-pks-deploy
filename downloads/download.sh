645186c6f2ca0ed15ab3614435f768c5a4ffd6a0
Tom Scanlan <tompscanlan@gmail.com>
2018-06-28 16:35:41 -0400

    add download script
    Signed-off-by: Tom Scanlan <tompscanlan@gmail.com>

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# first arg is the directory to put files
+
+cd $1
 curl -L  -o VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle "https://www.dropbox.com/s/n5pepfatetp55q2/VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle?dl=1"
 
 docker run -v ${PWD}:/vmwfiles apnex/myvmw
