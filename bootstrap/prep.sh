e392148c22b3e420fae84bb458b318e31d9b4457
Tom Hite <thite@vmware.com>
2018-08-06 13:10:28 -0700

    add packer build capability
    Signed-off-by: Tom Hite <thite@vmware.com>

--- bootstrap/prep.sh
+++ bootstrap/prep.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Pull in vars to setup various configurations
+. vars
+
+UPTODATE=
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
+    echo "This script must be run as root"
+    exit 1
 fi
 
 if [ -f /etc/os-release ]; then
@@ -16,12 +20,93 @@ else
     VER=$(uname -r)
 fi
 
-UPTODATE=
-
-if [ $OS = "Ubuntu" ] && [ $VER = "16.04" ]; then
-    apt-get update -qq
-    apt-get install docker.io git
+if [ "$OS" = "Ubuntu" ] && [ "$VER" = "16.04" ]; then
+    # don't reinstall docker
+    docker version >/dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        apt-get update -qq
+        apt-get install docker.io git golang-go
+    fi
     UPTODATE=1
+fi
+
+# setup variables for packer-ova-concourse only if it's available
+if [ -d ../../packer-ova-concourse ]; then
+
+cat >../../packer-ova-concourse/govc_import_concourse_ova.json <<EOF
+{
+  "NetworkMapping": [
+     {
+         "Name": "Network 1",
+         "Network": "${vsphere_network}"
+     }
+ ],
+  "DiskProvisioning": "thin"
+}
+EOF
+
+cat >../../packer-ova-concourse/variables.json <<EOF
+{
+  "bosh_stemcell_username": "${bosh_stemcell_username}",
+  "bosh_stemcell_password": "${bosh_stemcell_password}",
+  "vsphere_vm_name": "${vsphere_vm_name}",
+  "vsphere_ip_address": "${vsphere_ip_address}",
+  "vsphere_network": "${vsphere_network}",
+  "vsphere_netmask": "${vsphere_netmask}",
+  "vsphere_gateway": "${vsphere_gateway}",
+  "vsphere_nameserver": "${vsphere_nameserver}",
+  "vsphere_vcenter_server": "${vsphere_vcenter_server}",
+  "vsphere_username": "${vsphere_username}",
+  "vsphere_password": "${vsphere_password}",
+  "vsphere_datacenter": "${vsphere_datacenter}",              
+  "vsphere_cluster": "${vsphere_cluster}",
+  "vsphere_datastore": "${vsphere_datastore}",
+  "vsphere_resource_pool": "${vsphere_resource_pool}",
+  "vsphere_folder": "${vsphere_folder}",
+  "vsphere_insecure": "${vsphere_insecure}",
+  "vm_username": "${vm_username}",
+  "vm_password": "${vm_password}"
+}
+EOF
+
+cat >../../packer-ova-concourse/govc_import_concourse_ova.json <<EOF
+{
+  "NetworkMapping": [
+     {
+         "Name": "Network 1",
+         "Network": "${vsphere_network}"
+     }
+ ],
+  "DiskProvisioning": "thin",
+  "PropertyMapping": [
+        {
+            "Key": "ip0",
+            "Value": "${vsphere_ip_address}"
+        },
+        {
+            "Key": "netmask0",
+            "Value": "${vsphere_netmask}"
+        },
+        {
+            "Key": "gateway",
+            "Value": "${vsphere_gateway}"
+        },
+        {
+            "Key": "DNS",
+            "Value": "${vsphere_nameserver}"
+        },
+        {
+            "Key": "ntp_servers",
+            "Value": "${vsphere_ntp_servers}"
+        },
+        {
+            "Key": "admin_password",
+            "Value": "${vsphere_password}"
+        }
+    ]
+}
+EOF
+
 fi
 
 if [ -z "${UPTODATE}" ]; then
